89987cccd54704f62a257071a5bfdb0b60206763
FabianMosa <fabiancarab@gmail.com>
2025-10-30 10:52:40 -0300

    spi

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 // Creamos una instancia de Axios con la URL base de nuestra API
 console.log('API Base URL:', API_BASE_URL);
 
