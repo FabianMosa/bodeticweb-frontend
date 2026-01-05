ee2a3501d505b4b173ec152bd43407cce6dfdffd
FabianMosa <115504117+FabianMosa@users.noreply.github.com>
2026-01-05 12:24:28 -0300

    Change ITEMS_PER_PAGE from 20 to 15

--- src/pages/InventarioPage.jsx
+++ src/pages/InventarioPage.jsx
@@ -49,7 +49,7 @@ const InventarioPage = () => {
   // --- Estados para Paginación ---
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const ITEMS_PER_PAGE = 20;
+  const ITEMS_PER_PAGE = 15;
 
   // --- Estados de Modales ---
   const [salidaModalOpen, setSalidaModalOpen] = useState(false);
